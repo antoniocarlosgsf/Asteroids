77a2f790331cb15098dd154dcdcb2294a635022a
antoniocarlosgsf <antoniocarlos171201@gmail.com>
2023-11-22 11:16:11 -0300

    Progredindo nos sprites

--- sprites.py
+++ sprites.py
@@ -14,8 +14,35 @@ class Nave(pygame.sprite.Sprite):
         self.speedx = 0
         self.speedy = 0
         self.assets = assets
+        self.groups = groups
+
+        # Só será possível atirar uma vez a cada 300 milisegundos
+        self.last_shot = pygame.time.get_ticks()
+        self.shoot_ticks = 300
+
+    def update(self):
+        # Atualização da posição da nave
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+
+    def shoot(self):
+        now = pygame.time.get_tickes()
+        elapsed_ticks = now - self.last_shot
+
+        if elapsed_ticks > self.shoot_ticks:
+            # Marca o tick da nova imagem
+            self.last_shot = now
 
 class Meteor(pygame.sprite.Sprite):
+    def __init__(self, assets):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = assets[METEOR_IMG]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get.rect()
+        
+    
+
 
 
 class Bullet(pygame.sprite.Sprite):
