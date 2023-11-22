57848879a2fe425ad9a2793fcf5fb64a8af8715f
João Lucas Brasileiro <lucas_jl07@hotmail.com>
2023-11-22 18:57:50 -0300

    tela inicial + statusdojogo

--- init_screen.py
+++ init_screen.py
@@ -2,3 +2,20 @@ import pygame
 import random
 from os import path
 from config import IMG_DIR, BLACK, FPS, GAME
+
+def init_screen(screen):
+    #Ajuste da velocidade
+    clock = pygame.time.Clock()
+    
+    #Carregando a tela inicial
+    tela_inicial = pygame.image.load(path.join(IMG_DIR, 'tela_inicial.png')).convert()
+    tela_inicial_rect = tela_inicial.get_rect()
+
+    Statusjogo = True
+    while Statusjogo:
+        
+
+
+
+
+
