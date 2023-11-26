7e18f63ffe12bf916727b5b880abb4eb19838fa2
João Lucas Brasileiro <lucas_jl07@hotmail.com>
2023-11-25 23:49:40 -0300

    inserindo tela de game over

--- gameover_screen.py
+++ gameover_screen.py
@@ -5,3 +5,7 @@ from config import IMG_DIR, BLACK, FPS, GAME, QUIT
 
 
 def game_over_screen(screen):
+    #ajustando a velocidade
+    clock = pygame.time.Clock()
+    #carregando a tela de game over
+    tela_game_over = pygame.image.load(path.join(IMG_DIR, 'Se lascou doidim.png')).convert()
