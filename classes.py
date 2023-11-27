cac85cbd852a9320366589d03330ad2eb3ddb5d7
joao sardou <sardou_oliveira@hotmail.com>
2023-11-27 18:22:05 -0300

    "Atualizações do dia 27"

--- classes.py
+++ classes.py
@@ -1,137 +1,302 @@
 import pygame
+import math
+from config import *
 import random
-from funcoes import load_sprite
-from config import WIDTH, HEIGTH, METEOR_WIDTH, METEOR_HEIGTH, NAVE_WIDTH, NAVE_HEIGTH, BLACK, FPS
-from assets import SHIP_IMG, PEW_SOUND, METEOR_IMG, BULLET_IMG, EXPLOSION_ANIM
-from pygame.math import Vector2
-from pygame.transform import rotozoom
 
-UP = Vector2(0, -1)
-
-class Jogo:
+class Player(object):
     def __init__(self):
-        self.init_pygame()
-        self.tela = pygame.display.set_mode((WIDTH, HEIGTH))
-        self.background = pygame.transform.scale(load_sprite("background", False), (WIDTH, HEIGTH))
-        self.clock = pygame.time.Clock()
-        self.meteoro = Objetos((METEOR_WIDTH, METEOR_HEIGTH), load_sprite('meteoro1'), (1, 0))
-        self.nave = Nave((NAVE_WIDTH, NAVE_HEIGTH))
-
-    def main_loop(self):
-        while True:
-            self.event()
-            self.logica_jogo()
-            self.draw()
-
-    def _init_pygame(self):
-        pygame.init()
-        pygame.display.set_caption(" SPACE SHIP")
-
-    def event(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                quit()
-        
-        pressionada = pygame.key.get_pressed()
-
-        if pressionada[pygame.K_RIGHT]:
-            self.nave.rotate(horario=True)
-        elif pressionada[pygame.K_LEFT]:
-            self.nave.rotate(horario=False)
-        
-
-    def logica_jogo(self):
-        self.nave.updade()
-        self.meteoro.updade()
-
-    def draw(self):
-        self.screen.blit(self.background, (0, 0))
-        self.nave.draw(self.tela)
-        self.meteoro.draw(self.tela)
-        pygame.display.flip()
-        self.clock.tick(FPS)
-
-class Objetos:
-    def __init__(self, position, sprite, velocity):
-        self.position = Vector2(position)
-        self.sprite = sprite
-        self.radius = sprite.get_width() / 2
-        self.velocity = Vector2(velocity)
-
-    def draw(self, surface):
-        blit_position = self.position - Vector2(self.radius)
-        surface.blit(self.sprite, blit_position)
-
-    def updade(self):
-        self.position += self.velocity
-
-    def colisao(self, other_obj):
-        distance = self.position.distance_to(outro_obj.position)
-        return distance < self.radius + outro_obj.radius    
-
-class Nave(Objetos):
-    def __init__(self, posicao):
-        self.direcao = Vector2(UP)
-        super().__init__(posicao, load_sprite('Nave'), Vector2(0))
-    ROTACAO = 3
-
-    def rotate(self, horario=True):
-        sign = 1 if horario else -1
-        angulo = self.ROTACAO*sign
-        self.direction.rotate_ip(angulo)
-
-    def draw(self, surface):
-        angle = self.direction.angle_to(UP)
-        rotacionada = rotozoom(self.sprite, angle, 1.0)
-        tamanho_rotacionada = Vector2(rotacionada.get_size())
-        blit_position = self.position - tamanho_rotacionada*0.5
-        surface.blit(rotacionada, blit_position)
-
-        # Só será possível atirar uma vez a cada 300 milisegundos
-        self.last_shot = pygame.time.get_ticks()
-        self.shoot_ticks = 300
-
-    def update(self):
-        # Atualização da posição da nave
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-
-    def shoot(self):
-        now = pygame.time.get_tickes()
-        elapsed_ticks = now - self.last_shot
-
-        if elapsed_ticks > self.shoot_ticks:
-            # Marca o tick da nova imagem
-            self.last_shot = now
-
-class Meteor(pygame.sprite.Sprite):
-    def __init__(self, assets):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = assets[METEOR_IMG]
-        self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = random.randint(-METEOR_WIDTH, WIDTH+METEOR_WIDTH)
-        self.rect.y = random.randint(-100, HEIGTH+METEOR_HEIGTH)
-        while (self.rect.x >= 0 and self.rect.x <= WIDTH - METEOR_WIDTH) and (self.rect.y >= -METEOR_HEIGTH and self.rect.y <= HEIGTH + METEOR_HEIGTH):
-            self.rect.x = random.randint(-METEOR_WIDTH, WIDTH+METEOR_WIDTH)
-            self.rect.y = random.randint(-100, HEIGTH+METEOR_HEIGTH)
-        self.speedx = random.randint(-3, 3)
-        self.speedy = random. randint(2,9)
-
-    def update(self):
-        # Atualiza a posição do meteoro
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        # Se o meteoro sair da tela, é resorteada sua posição
-        if self.rect.top > HEIGTH or self.rect.rigth 
+        self.img = player
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+        self.x = width/2
+        self.y = height/2
         
         
+        
+        self.angle = 0
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine * self.width/2, self.y - self.sine * self.height/2)
+
+
+    #Método para desenhar o jogador na tela
+    def draw(self, win):
+        win.blit(self.rotatedSurf, self.rotatedRect)
+
+    def turnLeft(self):
+        self.angle += 5
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine * self.width/2, self.y - self.sine * self.height/2)
+
+        
+    def turnRight(self):
+        self.angle -= 5
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine * self.width/2, self.y - self.sine * self.height/2)
     
-
-
-
-class Bullet(pygame.sprite.Sprite):
+    def moveForward(self):
+        self.x += self.cosine * 6
+        self.y -= self.sine * 6
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine * self.width/2, self.y - self.sine * self.height/2)
+    # há a possibilidade de criar uma seta para trás 
     
+    def updateLocation(self):
+        if self.x > width + 50:
+            self.x = 0
+        elif self.x < 0 - self.width:
+            self.x = width
+        elif self.y < -50:
+            self.y = height
+        elif self.y > height + 50:
+            self.y = 0
 
-class Explosion(pygame.sprite.Sprite):
+
+#Definindo a classe para a bala
+class Bullet(object):
+    def __init__(self):
+        self.point = player.head
+        self.x, self.y = self.point
+        self.width = 4
+        self.height = 4
+        self.c = player.cosine
+        self.s = player.sine
+        self.xv = self.c*10
+        self.yv = self.s*10
     
+    
+    #Método para mover a bala
+    def move(self):
+        self.x += self.xv
+        self.y -= self.yv
+    
+    
+    #Método para desenhar a bala
+    def draw(self, win):
+        pygame.draw.rect(win, (255, 255, 255),[self.x, self.y, self.width, self.height])
+            #importante, não utilizo imagem da bala e sim o desenho dela
+    
+    
+    #Método para verificar se a bala está fora da tela
+    def checkoffScreen(self):
+        if self.x + self.width < 0 or self.x > width or self.y > height or self.y + self.height< 0:
+            return True 
+        
+class Asteroid(object):
+    def __init__(self, rank):
+        self.rank = rank
+        if self.rank == 1:
+            self.image = meteorS
+        
+        elif self.rank == 2:
+            self.image = meteorM
+            
+        else:
+            self.image = meteorB
+                    
+        self.width = 50 * rank
+        self.height = 50 * rank
+        #Esta linha cria um ponto de posição inicial aleatória para o asteroide, garantindo que ele comece fora da tela em uma das bordas.
+        self.ranPoint = random.choice([(random.randrange(0, width-self.width), random.choice([-1 * self.height - 5, height + 5])), (random.choice([-1 * self.width - 5, width + 5]), random.randrange(0, height - self.height))])
+        self.x, self.y = self.ranPoint
+        if self.x < width/2:
+            self.xdir = 1
+        else:
+            self.xdir = -1
+        if self.y < height/2:
+            self.ydir = 1
+        else:
+            self.ydir = -1
+        self.xv = self.xdir * random.randrange(1, 3)
+        self.yv = self.ydir * random.randrange(1, 3)
+    
+    def draw(self, win):
+        win.blit(self.image, (self.x, self.y))
+class Star(object):
+    def __init__(self):
+        self.img = star
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+        self.ranPoint = random.choice([(random.randrange(0, width-self.width), random.choice([-1 * self.height - 5, height + 5])), (random.choice([-1 * self.width - 5, width + 5]), random.randrange(0, height - self.height))])  
+        self.x, self.y = self.ranPoint
+        if self.x < width/2:
+            self.xdir = 1
+        else:
+            self.xdir = -1
+        if self.y < height/2:
+            self.ydir = 1
+        else:
+            self.ydir = -1
+        self.xv = self.xdir * 2
+        self.yv = self.ydir * 2
+    
+    def draw(self, win):
+        win.blit(self.img, (self.x, self.y))
+
+#Função para redesenhar a tela do jogo
+def redrawGameWindow():
+    win.blit(backgroud, (0,0))
+    font = pygame.font.SysFont('arial', 40) #Alterei para 40
+    livesText = font.render('Lives: ' + str(lives), 1, (255, 255, 255))
+    fontAgain = pygame.font.SysFont('arial', 60) #minha
+    playAgainText = fontAgain.render('Press Space to Play Again', 1, (255, 255, 255))
+    scoreText = font.render('Score: ' + str(score), 1, (255, 255, 255))
+
+
+    player.draw(win)
+    for a in asteroids:
+        a.draw(win)
+    for b in playerBullets:
+        b.draw(win)
+    for s in stars:
+        s.draw(win)
+    
+    if rapidFire:
+        pygame.draw.rect(win, (0, 0, 0), [width/2 - 51, 19, 102, 22])
+        pygame.draw.rect(win, (255, 255, 255), [width/2 - 50, 20, 100 - 100 * (count - rfStart)/500, 20])
+    
+    
+    if gameover:
+        win.blit(playAgainText, (width/2 -playAgainText.get_width()/2, height/2 - playAgainText.get_height()/2))
+    win.blit(scoreText, (width - scoreText.get_width() - 25, 25))
+    win.blit(livesText, (25, 25))
+    pygame.display.update()
+
+
+#Inicializando jogador e lista de balas
+player = Player()
+playerBullets= []
+asteroids = []
+count = 0
+stars = [Star()]
+#Loop principal do jogo
+run = True
+while run:
+    clock.tick(50)
+    count += 1
+    if not gameover:
+        if count % 50 == 0:
+            ran = random.choice([1, 1, 1, 2, 2, 3])
+            asteroids.append(Asteroid(ran))
+        
+        if count % 1000 == 0:
+            stars.append(Star())
+
+        player.updateLocation()
+        for b in playerBullets:
+            b.move()
+            if b.checkoffScreen():
+                playerBullets.pop(playerBullets.index(b))
+        
+        for a in asteroids:
+            a.x += a.xv
+            a.y += a.yv
+
+            if (player.x >= a.x and player.x <= a.x + a.width) or player.x + player.width >= a.x and player.x + player.width <= a.x +a.width:
+                if (player.y >= a.y and player.y <= a.y + a.height) or player.y + player.height >= a.y and player.y + player.height <= a.y + a.height:
+                    lives -= 1
+                    asteroids.pop(asteroids.index(a))
+                    break
+            
+            
+            for b in playerBullets:
+                if (b.x >= a.x and b.x <= a.x + a.width) or b.x + b.width >= a.x and b.x + b.width <= a.x +a.width:
+                    if (b.y >= a.y and b.y <= a.y + a.height) or b.y + b.height >= a.y and b.y + b.height <= a.y + a.height:
+                        if a.rank == 3:
+                            score += 10
+                            na1 = Asteroid(2)
+                            na2 = Asteroid(2)
+                            na1.x = a.x
+                            na2.x = a.x
+                            na1.y = a.y
+                            na2.y = a.y
+                            asteroids.append(na1)
+                            asteroids.append(na2)
+
+                        elif a.rank == 2:
+                            score += 20
+                            na1 = Asteroid(1)
+                            na2 = Asteroid(1)
+                            na1.x = a.x
+                            na2.x = a.x
+                            na1.y = a.y
+                            na2.y = a.y
+                            asteroids.append(na1)
+                            asteroids.append(na2)
+                        else:
+                            score += 30
+                        asteroids.pop(asteroids.index(a))
+                        playerBullets.pop(playerBullets.index(b))
+                        break
+
+
+        for s in stars:
+            s.x += s.xv
+            s.y += s.yv
+            if s.x < -100 - s.width or s.x > width + 100 or s.y > height + 100 or s.y < -100 - s.height:
+                stars.pop(stars.index(s))
+                break
+            for b in playerBullets:
+                if (b.x >= s.x and b.x <= s.x + s.width) or b.x + b.width >= s.x and b.x + b.width <= s.x +s.width:
+                    if (b.y >= s.y and b.y <= s.y + s.height) or b.y + b.height >= s.y and b.y + b.height <= s.y + s.height:
+                        rapidFire = True
+                        rfStart = count
+                        stars.pop(stars.index(s))
+                        playerBullets.pop(playerBullets.index(b))
+                        break
+        
+
+        if lives <= 0:
+            gameover = True
+        if rfStart != -1:
+            if count - rfStart > 500:
+                rapidFire = False
+                rfStart = -1
+
+
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.turnLeft()
+        if keys[pygame.K_RIGHT]:
+            player.turnRight()
+        if keys[pygame.K_UP]:
+            player.moveForward()
+        if keys[pygame.K_SPACE]:
+            if rapidFire:
+                playerBullets.append(Bullet())
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if not gameover:
+                    if not rapidFire:
+                        playerBullets.append(Bullet())
+                else:
+                    gameover = False
+                    lives = 3
+                    score = 0
+                    asteroids.clear()
+
+    
+    redrawGameWindow()
