d6186fd848aaa3e709cda7dc3f81e89347010cc1
joao sardou <sardou_oliveira@hotmail.com>
2023-11-28 19:59:18 -0300

    Atualização Config

--- config.py
+++ config.py
@@ -1,14 +1,9 @@
-#Biblioteca do Jogo
 import pygame
-#Tamanho da Janela  
+
+#Definindo o tamanho da tela
 width = 800
 height = 800
 
-gameover = False
-lives = 3
-score = 0
-rapidFire = False
-rfStart = -1
 
 #importando as imagens
 backgroud = pygame.image.load('img/starbg.png')
@@ -16,10 +11,22 @@ player = pygame.image.load('img/player.png')
 meteorB = pygame.image.load('img/meteor1.png') 
 meteorM = pygame.image.load('img/meteor2.png') 
 meteorS = pygame.image.load('img/meteor3.png') 
-enemy = pygame.image.load('img/enemy.png')
+#bullet = pygame.image.load('img/bullet.png')  #nao foi utilizada, criei a bala com o "draw"
 star = pygame.image.load('img/star.png')
 
 
 #Configurações da Tela
 pygame.display.set_caption('Asteroids') 
-win = pygame.display.set_mode((width, height)) 
+win = pygame.display.set_mode((width, height))
+
+#Definindo uma variável para o jogo
+gameover = False
+lives = 3
+score = 0
+rapidFire = False
+rfStart = -1
+
+SAIR = 0
+INICIAL = 1
+JOGANDO = 2
+FINAL = 3
